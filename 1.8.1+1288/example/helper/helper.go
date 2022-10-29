b369a672977c90e310e8d1329cef9b06fac83a91
Artem V. Navrotskiy <bozaro@yandex.ru>
2022-10-29 11:59:18 +0300

    Add helper

---
+++ 1.8.1+1288/example/helper/helper.go
@@ -0,0 +1,10 @@
+package helper
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func CheckTrace(t *testing.T) {
+	require.Fail(t, "Hello from helper!")
+}
--- 1.8.1/example/example_test.go
+++ 1.8.1/example/example_test.go
@@ -1,10 +1,15 @@
 package example
 
 import (
+	"github.com/bozaro/testify-example/example/helper"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestSimple(t *testing.T) {
 	require.Fail(t, "Hello, world!")
+}
+
+func TestHelper(t *testing.T) {
+	helper.CheckTrace(t)
 }
